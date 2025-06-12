a2e0d00e51da886d3c36dc6604d9633194d7042c
kappader4 <kappader4@gmail.com>
2025-06-12 20:59:44 +0200

    Reabrand from "Leaf" server to "Papyrus" server

---
+++ leaf-server/src/main/java/gg/pufferfish/pufferfish/util/IterableWrapper.java
@@ -0,0 +1,13 @@
+package gg.pufferfish.pufferfish.util;
+
+import java.util.Iterator;
+
+import org.jetbrains.annotations.NotNull;
+
+public record IterableWrapper<T>(Iterator<T> iterator) implements Iterable<T> {
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return iterator;
+    }
+}
---
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncTargetFinding.java
@@ -0,0 +1,59 @@
+package org.dreeam.leaf.config.modules.async;
+
+import org.dreeam.leaf.config.ConfigModules;
+import org.dreeam.leaf.config.EnumConfigCategory;
+import org.dreeam.leaf.config.LeafConfig;
+
+public class AsyncTargetFinding extends ConfigModules {
+
+    public String getBasePath() {
+        return EnumConfigCategory.ASYNC.getBaseKeyName() + ".async-target-finding";
+    }
+
+    public static boolean enabled = false;
+    public static boolean alertOther = true;
+    public static boolean searchBlock = true;
+    public static boolean searchEntity = true;
+    public static int queueSize = 4096;
+    public static long threshold = 10L;
+    private static boolean asyncTargetFindingInitialized;
+
+    @Override
+    public void onLoaded() {
+        config.addCommentRegionBased(getBasePath(), """
+                This moves the expensive entity and block search calculations to background thread while
+                keeping the actual validation on the main thread.""",
+            """
+                这会将昂贵的实体目标搜索计算移至后台线程, 同时在主线程上保持实际的实体验证.""");
+
+        if (asyncTargetFindingInitialized) {
+            config.getConfigSection(getBasePath());
+            return;
+        }
+        asyncTargetFindingInitialized = true;
+
+        enabled = config.getBoolean(getBasePath() + ".enabled", enabled);
+        // Disable if parallel world ticking is enabled, as they are incompatible.
+        if (enabled && SparklyPaperParallelWorldTicking.enabled) {
+            LeafConfig.LOGGER.warn("Async Target Finding is incompatible with Parallel World Ticking. Disabling Async Target Finding automatically.");
+            enabled = false;
+        }
+        alertOther = config.getBoolean(getBasePath() + ".async-alert-other", true);
+        searchBlock = config.getBoolean(getBasePath() + ".async-search-block", true);
+        searchEntity = config.getBoolean(getBasePath() + ".async-search-entity", true);
+        queueSize = config.getInt(getBasePath() + ".queue-size", 0);
+        threshold = config.getLong(getBasePath() + ".threshold", 0);
+
+        if (queueSize <= 0) {
+            queueSize = 4096;
+        }
+        if (threshold == 0L) {
+            threshold = 10L;
+        }
+        if (!enabled) {
+            alertOther = false;
+            searchEntity = false;
+            searchBlock = false;
+        }
+    }
+}
